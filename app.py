136e1fdb0f8a11e6d04234ef69acd7912e99cd85
1234-000 <wilsonm68@nycstudents.net>
2026-01-28 09:23:04 -0500

    WM

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
-print("tests")
+import turtle
+from turtle import *
+t = Turtle()
+
+
+def square(x):
+    t.forward(100)
+    t.left(90)
+    t.forward(100)
+    t.left(90)
+    t.forward(100)
+    t.left(90)
+    t.forward(100)
+    t.left(90)
+square(200)
+
+turtle.done()
