0852e96d322585e18a5af40ed960ab088b54558f
1234-000 <wilsonm68@nycstudents.net>
2026-02-02 08:50:36 -0500

    wm

--- app.py
+++ app.py
@@ -2,16 +2,31 @@ import turtle
 from turtle import *
 t = Turtle()
 
+def equal(x):
+    t.forward(90)
+    t.left(120)
+    t.forward(90)
+    t.left(120)
+    t.forward(90)
+equal(200) 
 
-def square(x):
-    t.forward(100)
+def rectangle(x):
+    t.forward(125)
     t.left(90)
     t.forward(100)
     t.left(90)
-    t.forward(100)
+    t.forward(125)
     t.left(90)
     t.forward(100)
     t.left(90)
-square(200)
+rectangle(200)
 
 turtle.done()
+
+def right():
+    t.forward(100)
+    t.left(90)
+    t.forward(100)
+    t.left(135)
+    t.forward(142)
+right()
---
+++ loops.py
@@ -0,0 +1,69 @@
+import turtle
+from turtle import *
+t = Turtle()
+t.speed(100)
+
+
+sidelength = 50
+rotate = 144
+def square(x,y):
+    for i in range(5):
+        t.forward(x)
+        t.right(144)
+square(5,144)
+
+def addSquares(iRange):
+    length = 25
+    for i in range(iRange):
+        square(length, 144)
+        length += 5
+        t.right(5)
+addSquares(60)
+
+
+turtle.done()
+
+def doubleSquares(iRange):
+    length = 25
+    for i in range(iRange):
+        square(length, 90)
+        length = length * 2
+doubleSquares(5)
+
+
+
+sidelength = 5
+rotate = 90
+def square(x,y):
+    for i in range(4):
+        t.forward(x)
+        t.left(y)
+square(5,90)
+
+def addSquares(iRange):
+    length = 25
+    for i in range(iRange):
+        square(length, 90)
+        length += 5
+        t.right(5)
+addSquares(60)
+
+
+
+
+sidelength = 100
+rotate = 90
+def square(x,y):
+    for i in range(4):
+        t.forward(x)
+        t.left(y)
+square(100,90)
+
+def addSquares(iRange):
+    length = 100
+    for i in range(iRange):
+        square(length, 90)
+        t.left(5)
+addSquares(60)
+
+
